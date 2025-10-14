667f7387a68556e05cb859339c392b37a776359a
MaxSummerX <marspowerpops@gmail.com>
2025-10-14 12:14:11 +0300

    Created CRUD for route products and connected to the main application

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
-from app.routers.categories import router
+from app.routers import categories, products
 
 
 # Создаём приложение FastAPI
 app = FastAPI(title="FastAPI интернет-магазин", version="0.1.0")
 
 # Подключаем маршруты категорий
-app.include_router(router)
+app.include_router(categories.router)
+app.include_router(products.router)
 
 
 # Корневой эндпойнт для проверки
