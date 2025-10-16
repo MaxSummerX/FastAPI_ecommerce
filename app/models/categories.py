60b21c7e6e1cc1330de42a42aa394c1a2bdd34e5
MaxSummerX <marspowerpops@gmail.com>
2025-10-16 12:14:15 +0300

    Added self-referencing links for categories

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Boolean, String
+from sqlalchemy import Boolean, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -16,6 +16,13 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    parent_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=True)
 
     # Создаём связь через products к category
     products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
+
+    # Создаём самоссылающеюся связь для родителя parent к children
+    parent: Mapped["Category | None"] = relationship("Category", back_populates="children", remote_side="Category.id")
+
+    # Создаём самоссылающеюся связь для наследника children к parent
+    children: Mapped[list["Category"]] = relationship("Category", back_populates="parent")
