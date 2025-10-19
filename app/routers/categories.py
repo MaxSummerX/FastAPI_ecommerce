3082ea14fccdc373906ef3ded781bd560c6a457e
MaxSummerX <marspowerpops@gmail.com>
2025-10-20 00:25:55 +0300

    Added GET and POST endpoints for products and categories

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,23 +1,50 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.depends.db_depends import get_db
+from app.models.categories import Category as CategoryModel
+from app.schemas.categories import Category as CategorySchema
+from app.schemas.categories import CategoryCreate
 
 
 router = APIRouter(prefix="/categories", tags=["categories"])
 
 
-@router.get("/")
-async def get_all_categories() -> dict:
+@router.get("/", response_model=list[CategorySchema])
+async def get_all_categories(db: Session = Depends(get_db)) -> Any:
     """
     Возвращает список всех категорий товаров.
     """
-    return {"message": "Список всех категорий (заглушка)"}
+    stmt = select(CategoryModel).where(CategoryModel.is_active)
+    category = db.scalars(stmt).all()
+
+    return category
 
 
-@router.post("/")
-async def create_category() -> dict:
+@router.post("/", response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
+async def create_category(category: CategoryCreate, db: Session = Depends(get_db)) -> Any:
     """
     Создаёт новую категорию.
     """
-    return {"message": "Категория создана (заглушка)"}
+    # Проверяем существования parent_id, если указан
+    if category.parent_id is not None:
+        # Формируем запрос
+        stmt = select(CategoryModel).where(CategoryModel.id == category.parent_id, CategoryModel.is_active)
+        # Отправляем запрос
+        parent = db.scalars(stmt).first()
+        #  Проверяем результат запроса
+        if parent is None:
+            raise HTTPException(status_code=400, detail="Parent category not found")
+
+    # Создание новой категории
+    db_category = CategoryModel(**category.model_dump())
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
 
 
 @router.put("/category_id")
--- app/routers/products.py
+++ app/routers/products.py
@@ -1,23 +1,45 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.depends.db_depends import get_db
+from app.models.categories import Category as CategoryModel
+from app.models.products import Product as ProductModel
+from app.schemas.products import Product as ProductSchema
+from app.schemas.products import ProductCreate
 
 
 router = APIRouter(prefix="/products", tags=["products"])
 
 
-@router.get("/")
-async def get_all_products() -> dict:
+@router.get("/", response_model=list[ProductSchema])
+async def get_all_products(db: Session = Depends(get_db)) -> Any:
     """
     Возвращает список всех товаров.
     """
-    return {"message": "Список всех товаров (заглушка)"}
+    stmt = select(ProductModel).where(ProductModel.is_active)
+    products = db.scalars(stmt).all()
+    return products
 
 
-@router.post("/")
-async def create_product() -> dict:
+@router.post("/", response_model=ProductSchema, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductCreate, db: Session = Depends(get_db)) -> Any:
     """
     Создаёт новый товар.
     """
-    return {"message": "товар создан (заглушка)"}
+    # Проверяем существует ли категория
+    stmt = select(CategoryModel).where(CategoryModel.id == product.category_id)
+    category = db.scalars(stmt).first()
+    if category is None:
+        raise HTTPException(status_code=400, detail="Category not found")
+
+    db_product = ProductModel(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
 
 
 @router.get("/category/{category_id}")
