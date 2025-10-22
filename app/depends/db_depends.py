f5ac888a25f64665639ae197bac7c2ac8ca02731
MaxSummerX <marspowerpops@gmail.com>
2025-10-22 13:41:33 +0300

    An asynchronous session generator has been added

--- app/depends/db_depends.py
+++ app/depends/db_depends.py
@@ -1,8 +1,9 @@
-from collections.abc import Generator
+from collections.abc import AsyncGenerator, Generator
 
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
-from app.database import SessionLocal
+from app.database import SessionLocal, async_session_maker
 
 
 def get_db() -> Generator[Session]:
@@ -15,3 +16,14 @@ def get_db() -> Generator[Session]:
         yield db
     finally:
         db.close()
+
+
+# --------------- Асинхронная сессия -------------------------
+
+
+async def get_async_db() -> AsyncGenerator[AsyncSession]:
+    """
+    Предоставляет асинхронную сессию SQLAlchemy для работы с базой данных PostgreSQL.
+    """
+    async with async_session_maker() as session:
+        yield session
