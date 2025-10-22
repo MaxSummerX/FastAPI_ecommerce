d2b3f354c8c611710a0273b2b6f10fe9aa27f560
MaxSummerX <marspowerpops@gmail.com>
2025-10-22 13:39:41 +0300

    Added asynchronous connection to PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,6 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+
+load_dotenv()
 
 # Строка подключения для SQLite
 DATABASE_URL = "sqlite:///ecommerce.db"
@@ -10,6 +16,16 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 # Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
+
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = os.environ["POSTGRESQL"]
+
+# Создаём engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 
 # Определяем базовый класс для моделей
