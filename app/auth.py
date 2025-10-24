66f1cb1a35ab369569b65382ff38caa5d5bc2d5a
MaxSummerX <marspowerpops@gmail.com>
2025-10-24 16:18:12 +0300

    Added a function for creating a refresh_token

--- app/auth.py
+++ app/auth.py
@@ -14,6 +14,8 @@ from app.models.users import User as UserModel
 
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
@@ -47,6 +49,20 @@ def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     # Вычисляет время истечения токена
     expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # Добавляет поле exp (expiration) в payload токена
+    to_encode.update({"exp": expire})
+    # Возвращаем строку токена
+    return cast(str, jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM))
+
+
+def create_refresh_token(data: dict) -> str:
+    """
+    Создаёт рефреш-токен с длительным сроком действия.
+    """
+    # Создаём копию входного словаря
+    to_encode = data.copy()
+    # Вычисляет время истечения токена
+    expire = datetime.now(UTC) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     # Добавляет поле exp (expiration) в payload токена
     to_encode.update({"exp": expire})
     # Возвращаем строку токена
